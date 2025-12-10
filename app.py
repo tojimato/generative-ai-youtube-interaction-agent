a088460d453f545bd112fb08b22e501a94d3f767
Tolga Kök <tojimato@gmail.com>
2025-12-10 16:02:54 +0300

    youtube search tool added

--- app.py
+++ app.py
@@ -48,3 +48,70 @@ print("----------------------------")
 print(extract_video_id.description)
 print("----------------------------")
 print(extract_video_id.func)
+
+print(extract_video_id.run("https://www.youtube.com/watch?v=hfIUstzHs9A"))
+
+tools = []
+tools.append(extract_video_id)
+
+from youtube_transcript_api import YouTubeTranscriptApi
+
+@tool
+def fetch_transcript(video_id: str, language: str = "en") -> str:
+    """
+    Fetches the transcript of a YouTube video.
+    
+    Args:
+        video_id (str): The YouTube video ID (e.g., "dQw4w9WgXcQ").
+        language (str): Language code for the transcript (e.g., "en", "es").
+    
+    Returns:
+        str: The transcript text or an error message.
+    """
+    
+    try:
+        ytt_api = YouTubeTranscriptApi()
+        transcript = ytt_api.fetch(video_id, languages=[language])
+        return " ".join([snippet.text for snippet in transcript.snippets])
+    except Exception as e:
+        return f"Error: {str(e)}"
+    
+
+print(fetch_transcript.run("hfIUstzHs9A"))
+
+tools.append(fetch_transcript)
+
+from pytube import Search
+from langchain.tools import tool
+from typing import List, Dict
+
+@tool
+def search_youtube(query: str) -> List[Dict[str, str]]:
+    """
+    Search YouTube for videos matching the query.
+    
+    Args:
+        query (str): The search term to look for on YouTube
+        
+    Returns:
+        List of dictionaries containing video titles and IDs in format:
+        [{'title': 'Video Title', 'video_id': 'abc123'}, ...]
+        Returns error message if search fails
+    """
+    try:
+        s = Search(query)
+        return [
+            {
+                "title": yt.title,
+                "video_id": yt.video_id,
+                "url": f"https://youtu.be/{yt.video_id}"
+            }
+            for yt in s.results
+        ]
+    except Exception as e:
+        return f"Error: {str(e)}"
+    
+search_out=search_youtube.run("Generative AI")
+display(JSON(search_out))
+
+tools.append(search_youtube)
